763bb1be5f7938f17d8085d7956c4d5387e988db
Bart Huylebroeck <bart.huylebroeck@pandora.be>
2018-12-02 21:49:37 +0100

    clones now ignore being shot

--- Assets/Player/PlayerInput.cs
+++ Assets/Player/PlayerInput.cs
@@ -36,6 +36,12 @@ public class PlayerInput : MonoBehaviour {
 
     void OnCollisionEnter(Collision collision)
     {
+        if (!this.enabled)
+        {
+            Debug.Log("ignoring collision during replay");
+            return;
+        }
+
         Debug.Log("player crashed into " + collision.gameObject.name);
         _onPlayerDeath.Invoke();
         GetComponent<Rigidbody>().isKinematic = true;
